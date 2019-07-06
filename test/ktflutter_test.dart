c9adb436225e25de4686daf27c60974e9bae25fe
rarnu <hexiaojie@hujiang.com>
2019-07-07 00:03:16 +0800

    add: regex

--- test/ktflutter_test.dart
+++ test/ktflutter_test.dart
@@ -5,13 +5,14 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:ktflutter/list_extension.dart';
 import 'package:ktflutter/map_extension.dart';
 import 'package:ktflutter/pair_extension.dart';
+import 'package:ktflutter/regex_extension.dart';
 import 'package:ktflutter/set_extension.dart';
 import 'package:ktflutter/string_extension.dart';
 
 void main() {
-    test('adds one to input values', () {
-        var list = ['two', 'one', 'three', 'four'];
-        list.sort((a, b) => a.length.compareTo(b.length));
-        list.forEach(print);
+    test('reg', () {
+        var str = "rarnu1985gmail.com";
+        var ret = isEmail(str);
+        print("regTest: ret => $ret");
     });
 }
